dbd01f77e75fcede6ff4f30b16a7b2269769751e
sowmisarwin <sowmimca9297@gmail.com>
2021-01-09 22:08:14 +0530

    updated

--- src/org/git/FirstGit.java
+++ src/org/git/FirstGit.java
@@ -3,5 +3,9 @@ package org.git;
 public class FirstGit {
 public static void main(String[] args) {
 	System.out.println("Sample git program");
+	System.out.println("SAMPLE PGM:");
+	int a=10;
+	int b=20;
+	System.out.println(a+b);
 }
 }
