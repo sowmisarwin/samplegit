e09ef38953228aeb168a0ab8720e466c071fc2f7
sowmisarwin <sowmimca9297@gmail.com>
2021-01-11 17:55:44 +0530

    EEEEEEEE

--- src/org/git/FirstGit.java
+++ src/org/git/FirstGit.java
@@ -5,11 +5,4 @@ public static void main(String[] args) {
 	System.out.println("Sample git program");
 	System.out.println("SAMPLE PGM:");
 	int a=10;
-	int b=20;
-	int c;
-	System.out.println("result");
-	System.out.println(c=a+b);
-	
-	
-}
-}
+	int b=20;}}
