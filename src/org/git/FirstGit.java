cd0dd63cd9ca501738286aa188e5817bb8d2368f
sowmisarwin <sowmimca9297@gmail.com>
2021-01-11 17:04:32 +0530

    new

--- src/org/git/FirstGit.java
+++ src/org/git/FirstGit.java
@@ -6,8 +6,9 @@ public static void main(String[] args) {
 	System.out.println("SAMPLE PGM:");
 	int a=10;
 	int b=20;
+	System.out.println(a+b);
 	System.out.println("result");
-	int c = a+b;
+	
 	
 }
 }
