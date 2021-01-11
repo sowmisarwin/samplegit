7dcefff51979eee1e6f1e61c5962fa3307728eaf
sowmisarwin <sowmimca9297@gmail.com>
2021-01-11 17:28:26 +0530

    cccc

--- src/org/git/FirstGit.java
+++ src/org/git/FirstGit.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	System.out.println("SAMPLE PGM:");
 	int a=10;
 	int b=20;
-	System.out.println(a+b-b);
+	System.out.println(a+b);
 	System.out.println("result");
 	
 	
